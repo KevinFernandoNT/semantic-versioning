68ac1ef6135d5ad214bacf95e989e90f2521fcc9
KevinFernandoNT <kevin@nuclei.tech>
2023-05-19 15:57:46 +0530

    release configs

--- release.config.js
+++ release.config.js
@@ -1,36 +1,36 @@
-// const config = {
-//   branches: ["main"],
-//   plugins: [
-//     "@semantic-release/commit-analyzer",
-//     "@semantic-release/release-notes-generator",
-//     [
-//       "@semantic-release/changelog",
-//       {
-//         changelogFile: "CHANGELOG.md",
-//         changelogTitle: "# Changelog\n\n",
-//       },
-//     ],
-//     [
-//       "@semantic-release/exec",
-//       {
-//         prepareCmd: "echo 'Release prepared'",
-//       },
-//     ],
-//     [
-//       "@semantic-release/git",
-//       {
-//         assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
-//         message: "chore(release): ${nextRelease.version} [skip ci]",
-//       },
-//     ],
-//     [
-//       "@semantic-release/github",
-//       {
-//         successComment: false,
-//         failComment: false,
-//       },
-//     ],
-//   ],
-// };
+const config = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog\n\n",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "echo 'Release prepared'",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failComment: false,
+      },
+    ],
+  ],
+};
 
-// module.exports = config;
+module.exports = config;
