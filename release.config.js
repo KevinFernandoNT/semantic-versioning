8d39d0d65281e20007de6e3fde5c07c0d7f7739a
KevinFernandoNT <kevin@nuclei.tech>
2023-05-20 12:15:08 +0530

    feat: remvove release rules

--- release.config.js
+++ release.config.js
@@ -2,15 +2,6 @@ const config = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    {
-      releaseRules: [
-        { type: "fix", release: "minor" },
-        { type: "feat", release: "minor" },
-        { type: "refactor", release: "minor" },
-        { type: "refactor", release: "patch" },
-        { scope: "no-release", release: false },
-      ],
-    },
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
