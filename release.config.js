dba982eb6057b8984975cdbda2b0bd9809ce9226
KevinFernandoNT <kevin@nuclei.tech>
2023-05-20 16:23:18 +0530

    added back configurations

--- release.config.js
+++ release.config.js
@@ -3,26 +3,26 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // [
-    //   "@semantic-release/changelog",
-    //   {
-    //     changelogFile: "CHANGELOG.md",
-    //     changelogTitle: "# Changelog\n\n",
-    //   },
-    // ],
-    // [
-    //   "@semantic-release/exec",
-    //   {
-    //     prepareCmd: "echo 'Release prepared'",
-    //   },
-    // ],
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
-    //     message: "chore(release): ${nextRelease.version} [skip ci]",
-    //   },
-    // ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog\n\n",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "echo 'Release prepared'",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
 
     [
       "@semantic-release/github",
