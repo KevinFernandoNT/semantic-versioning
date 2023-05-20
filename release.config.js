6f221d373f2da808faab613f1ff011407ba9b1fb
KevinFernandoNT <kevin@nuclei.tech>
2023-05-20 10:45:06 +0530

    added commit analyzer

--- release.config.js
+++ release.config.js
@@ -2,6 +2,15 @@ const config = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
+    {
+      preset: "angular",
+      releaseRules: [
+        { type: "fix", release: "minor" },
+        { type: "refactor", release: "minor" },
+        { type: "refactor", release: "patch" },
+        { scope: "no-release", release: true },
+      ],
+    },
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
@@ -23,6 +32,7 @@ const config = {
         message: "chore(release): ${nextRelease.version} [skip ci]",
       },
     ],
+    "@semantic-release/github",
     [
       {
         successComment: false,
