8bb0136c5d4ae4642619047af4bda417d25f4632
KevinFernandoNT <kevin@nuclei.tech>
2023-05-20 16:23:00 +0530

    added configs

--- release.config.js
+++ release.config.js
@@ -1,46 +1,37 @@
-// const config = {
-//   branches: ["main"],
-//   plugins: [
-//     "@semantic-release/commit-analyzer",
-//     {
-//       preset: "angular",
-//       releaseRules: [
-//         { type: "feat", release: "major" },
-//         { type: "fix", release: "patch" },
-//         { type: "refactor", release: "major" },
-//       ],
-//       parserOpts: {},
-//     },
-//     "@semantic-release/release-notes-generator",
-//     // [
-//     //   "@semantic-release/changelog",
-//     //   {
-//     //     changelogFile: "CHANGELOG.md",
-//     //     changelogTitle: "# Changelog\n\n",
-//     //   },
-//     // ],
-//     // [
-//     //   "@semantic-release/exec",
-//     //   {
-//     //     prepareCmd: "echo 'Release prepared'",
-//     //   },
-//     // ],
-//     // [
-//     //   "@semantic-release/git",
-//     //   {
-//     //     assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
-//     //     message: "chore(release): ${nextRelease.version} [skip ci]",
-//     //   },
-//     // ],
+const config = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    // [
+    //   "@semantic-release/changelog",
+    //   {
+    //     changelogFile: "CHANGELOG.md",
+    //     changelogTitle: "# Changelog\n\n",
+    //   },
+    // ],
+    // [
+    //   "@semantic-release/exec",
+    //   {
+    //     prepareCmd: "echo 'Release prepared'",
+    //   },
+    // ],
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
+    //     message: "chore(release): ${nextRelease.version} [skip ci]",
+    //   },
+    // ],
 
-//     [
-//       "@semantic-release/github",
-//       {
-//         successComment: false,
-//         failComment: false,
-//       },
-//     ],
-//   ],
-// };
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failComment: false,
+      },
+    ],
+  ],
+};
 
-// module.exports = config;
+module.exports = config;
